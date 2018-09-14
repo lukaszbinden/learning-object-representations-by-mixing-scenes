a9cd3b2f389987c3bfc4b8077c3a11da7bbb4191
Lukas Zbinden <lukas.zbinden@unifr.ch>
2018-09-14 10:46:06 +0200

    9_slices with overlap

--- src/constants.py
+++ src/constants.py
@@ -4,3 +4,5 @@ JSON_FILE_PARAM = '-p='
 JSON_FILE_DEFAULT = 'params.json'
 COMMENT_PARAM = '-c='
 LOG_FILE_NAME = 'main.log'
+
+NUM_TILES = 9
--- src/playground/9_slices.py
+++ src/playground/9_slices.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import imageio
 from skimage.io import imsave
 
-files = ['..\\images\\lake.png', '..\\images\\field.png']
+files = ['..\\images\\living_room.jpg', '..\\images\\cat_and_dog.jpg']
 patch_size = [300, 300]
 num_imgs = 2
 
@@ -21,77 +21,50 @@ outputs = tf.concat([outputs[:0], img_1, outputs[0 + 1:]], axis=0)
 outputs = tf.concat([outputs[:1], img_2, outputs[1 + 1:]], axis=0)
 print('outputs: ' + str(outputs.get_shape()))
 
-overlap = 10
-side_length = 300
+overlap = 30 # hyperparameter
+# assert overlap, 'hyperparameter \'overlap\' is not an integer'
+image_size = 300
+slice_size = (image_size + 2 * overlap) / 3
+slice_size_overlap = slice_size - overlap
+assert slice_size.is_integer(), 'hyperparameter \'overlap\' invalid'
+slice_size = int(slice_size)
+slice_size_overlap = int(slice_size_overlap)
 
-tiles_t1 = tf.image.crop_to_bounding_box(outputs, 0, 30, 150, 150)
-tiles_t2 = tf.image.crop_to_bounding_box(outputs, 50, 30, 150, 150)
-# tiles_t3 = None
-# tiles_t4 = None
-# tiles_t5 = None
-# tiles_t6 = None
-# tiles_t7 = None
-# tiles_t8 = None
-# tiles_t9 = None
+tile_r1c1 = tf.image.crop_to_bounding_box(outputs, 0, 0, slice_size, slice_size)
+tile_r1c2 = tf.image.crop_to_bounding_box(outputs, 0, slice_size_overlap, slice_size, slice_size)
+tile_r1c3 = tf.image.crop_to_bounding_box(outputs, 0, image_size - slice_size, slice_size, slice_size)
+tile_r2c1 = tf.image.crop_to_bounding_box(outputs, slice_size_overlap, 0, slice_size, slice_size)
+tile_r2c2 = tf.image.crop_to_bounding_box(outputs, slice_size_overlap, slice_size_overlap, slice_size, slice_size)
+tile_r2c3 = tf.image.crop_to_bounding_box(outputs, slice_size_overlap, image_size - slice_size, slice_size, slice_size)
+tile_r3c1 = tf.image.crop_to_bounding_box(outputs, image_size - slice_size, 0, slice_size, slice_size)
+tile_r3c2 = tf.image.crop_to_bounding_box(outputs, image_size - slice_size, slice_size_overlap, slice_size, slice_size)
+tile_r3c3 = tf.image.crop_to_bounding_box(outputs, image_size - slice_size, image_size - slice_size, slice_size, slice_size)
 
-
-
-
-#print(type(image))
-# <class 'tensorflow.python.framework.ops.Tensor'>
-#print(image.get_shape())
-# (?, ?, 3)
-
-# shape= tf.shape(img_1)
-# image_height = shape[0]
-# image_width = shape[1]
-# image_channels = shape[2]
-# print(image_height)
-# print(image_width)
-# print(image_channels)
-# #num_pieces = tf.div(image_width, image_height)
-# num_pieces = 2
-#
-# # We get the image patch for the given index:
-# sub_image = tf.image.crop_to_bounding_box(img_1, 0, 10 + (index * 100), 100, 100)
-# print('sub_image -->')
-# print(type(sub_image))
-# print(sub_image.get_shape())
-# print('sub_image <--')
-#
-# sub_image = tf.expand_dims(sub_image, 0)
-# outputs = tf.concat([outputs[:index], sub_image, outputs[index + 1:]], axis=0)
-# print(outputs.get_shape())
-# outputs.set_shape([2, 100, 100, 3])
-# print(outputs.get_shape())
-
-
-    # We build our patches tensor which will be filled with the loop:
-    # patches = tf.zeros((num_pieces, *patch_size, shape[2]), dtype=tf.uint8)
-    #_, patches = tf.while_loop(tf_while_condition, tf_while_body, [0, patches])
-
-    # We tile the label to have one per patch:
-    # patches_labels = tf.tile(tf.expand_dims(label, 0), [num_pieces])
-
-    # return tf.data.Dataset.from_tensor_slices((patches, patches_labels))
-
-
-#dataset = tf.data.Dataset.from_tensor_slices((filenames, labels))
-#dataset = dataset.flat_map(parse_fn)
 
 sess = tf.InteractiveSession()
-##it = dataset.make_one_shot_iterator()
-#op = it.get_next()
 
 print('sess.run..')
-print('tiles_t1 shape: ' + str(tiles_t1.get_shape()))
-tiles_t1, tiles_t2 = sess.run([tiles_t1,tiles_t2])
-print(type(tiles_t1))
-print(tiles_t1.shape)
-for idx, img_1 in enumerate(tiles_t1):
-    imageio.imwrite(('..\\images\\slices\\slices_t1_%s.png' % idx), img_1)
-
-for idx, img_1 in enumerate(tiles_t2):
-    imageio.imwrite(('..\\images\\slices\\slices_t2_%s.png' % idx), img_1)
+print('tile_r1c1 shape: ' + str(tile_r1c1.get_shape()))
+tile_r1c1,tile_r1c2,tile_r1c3,tile_r2c1,tile_r2c2,tile_r2c3,tile_r3c1,tile_r3c2,tile_r3c3 = sess.run([tile_r1c1,tile_r1c2,tile_r1c3,tile_r2c1,tile_r2c2,tile_r2c3,tile_r3c1,tile_r3c2,tile_r3c3])
+print(type(tile_r1c1))
+print(tile_r1c1.shape)
+for idx, img_1 in enumerate(tile_r1c1):
+    imageio.imwrite(('..\\images\\slices\\tile_r1c1_%s.png' % idx), img_1)
+for idx, img_1 in enumerate(tile_r1c2):
+    imageio.imwrite(('..\\images\\slices\\tile_r1c2_%s.png' % idx), img_1)
+for idx, img_1 in enumerate(tile_r1c3):
+    imageio.imwrite(('..\\images\\slices\\tile_r1c3_%s.png' % idx), img_1)
+for idx, img_1 in enumerate(tile_r2c1):
+    imageio.imwrite(('..\\images\\slices\\tile_r2c1_%s.png' % idx), img_1)
+for idx, img_1 in enumerate(tile_r2c2):
+    imageio.imwrite(('..\\images\\slices\\tile_r2c2_%s.png' % idx), img_1)
+for idx, img_1 in enumerate(tile_r2c3):
+    imageio.imwrite(('..\\images\\slices\\tile_r2c3_%s.png' % idx), img_1)
+for idx, img_1 in enumerate(tile_r3c1):
+    imageio.imwrite(('..\\images\\slices\\tile_r3c1_%s.png' % idx), img_1)
+for idx, img_1 in enumerate(tile_r3c2):
+    imageio.imwrite(('..\\images\\slices\\tile_r3c2_%s.png' % idx), img_1)
+for idx, img_1 in enumerate(tile_r3c3):
+    imageio.imwrite(('..\\images\\slices\\tile_r3c3_%s.png' % idx), img_1)
 
 print('...done.')
