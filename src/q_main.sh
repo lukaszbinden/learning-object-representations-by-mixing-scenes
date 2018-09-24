ad11c6350761deda9f53145c736ed1fac96c99a1
Lukas Zbinden <lukas.zbinden@unifr.ch>
2018-09-24 09:35:54 +0200

    initial commit

---
+++ src/q_main.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#$ -N main.py
+#$ -S /bin/bash
+#$ -cwd
+#$ -j y
+#$ -pe smp 1
+
+# Array job: -t <range>
+#$ -t 1
+
+#$ -l h=node06
+
+#$ -v DISPLAY
+
+#$ -o /data/cvg/lukas/learning-object-representations-by-mixing-scenes/logs/main.log
+
+#$ -m ea
+#$ -M lukas.zbinden@unifr.ch
+
+python main.py
