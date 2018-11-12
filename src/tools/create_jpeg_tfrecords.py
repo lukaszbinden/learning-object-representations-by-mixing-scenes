c357e8354a0db27f142049e8cdaccd2167cd9b66
lukaszbinden <lukas.zbinden@unifr.ch>
2018-11-12 17:03:27 +0100

    ensure same seed everywhere

--- src/tools/create_jpeg_tfrecords.py
+++ src/tools/create_jpeg_tfrecords.py
@@ -418,7 +418,7 @@ def _find_image_files(name, data_dir):
   # random ordering of the images with respect to label in the
   # saved TFRecord files. Make the randomization repeatable.
   shuffled_index = list(range(len(filenames)))
-  random.seed(12345)
+  random.seed(4285)
   random.shuffle(shuffled_index)
 
   filenames = [filenames[i] for i in shuffled_index]
