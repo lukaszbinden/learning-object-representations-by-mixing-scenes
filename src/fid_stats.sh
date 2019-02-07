fa055e33c3d7b79f0a392a2f702099406e3bfb2c
lukaszbinden <lukas.zbinden@unifr.ch>
2019-02-07 16:41:30 +0100

    add FID stats script

---
+++ src/fid_stats.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+# grep "\"fid\"" logs/$1/metrics/results/log*json | awk '{print $3}' | awk -F, '{print $1}' | sort -rn
+
+echo -e "\nNumber of epochs:"
+grep "\"fid\"" logs/$1/metrics/results/log*json | wc -l
+
+echo -e "\nStats of last 10 FID values:"
+grep "\"fid\"" logs/$1/metrics/results/log*json | awk '{print $3}' | awk -F, '{print $1}' | tail -10 | awk 'NR == 1 { max=$1; min=$1; sum=0 } { if ($1>max) max=$1; if ($1<min) min=$1; sum+=$1;} END {printf "Min: %f\tMax: %f\tAverage: %f\n", min, max, sum/NR}'
+
+echo -e ""
