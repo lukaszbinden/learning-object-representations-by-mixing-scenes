445d2245ef6746b088ab48c2e23f5068621e4992
lukaszbinden <lukas.zbinden@unifr.ch>
2019-02-11 15:35:02 +0100

    AE: added systematic, explicit name scoping; add new model "FC-DenseNet-RF-46" with RF=46px; add option for CoordConv layer at beginning of encoder;

--- src/autoencoder_dblocks.py
+++ src/autoencoder_dblocks.py
@@ -4,25 +4,31 @@
 from __future__ import division
 import tensorflow.contrib.slim as slim
 from ops_alex import *
+from ops_coordconv import coord_conv
 from constants import *
 
-def preact_conv(inputs, n_filters, kernel_size=[3, 3], dropout_p=0.2):
+def preact_conv(inputs, n_filters, kernel_size=[3, 3], dropout_p=0.2, scope="def"):
     """
     Basic pre-activation layer for DenseNets
     Apply successivly BatchNormalization, ReLU nonlinearity, Convolution and
     Dropout (if dropout_p > 0) on the inputs
     """
     # preact = slim.batch_norm(inputs, fused=True)
+
+    # comment in only for RF calculation!!!
     preact = instance_norm(inputs)
+    # comment out only for RF calculation!!!
+    # preact = inputs
     preact = tf.nn.relu(preact)
+
     conv = slim.conv2d(preact, n_filters, kernel_size, activation_fn=None, normalizer_fn=None,
-                       weights_initializer=tf.random_normal_initializer(stddev=0.02, seed=4285), biases_initializer=tf.constant_initializer(0.01))
+                       weights_initializer=tf.random_normal_initializer(stddev=0.02, seed=4285), biases_initializer=tf.constant_initializer(0.01), scope=scope + "_co")
     if dropout_p != 0.0:
-      conv = slim.dropout(conv, keep_prob=(1.0-dropout_p))
+      conv = slim.dropout(conv, keep_prob=(1.0-dropout_p), scope=scope + "_do")
     return conv
 
 
-def preact_conv_dec(inputs, n_filters, kernel_size=[3, 3], dropout_p=0.2, name=None):
+def preact_conv_dec(inputs, n_filters, kernel_size=[3, 3], dropout_p=0.2, name="def"):
     """
     Basic pre-activation layer for DenseNets
     Apply successivly BatchNormalization, ReLU nonlinearity, Convolution and
@@ -35,15 +41,15 @@ def preact_conv_dec(inputs, n_filters, kernel_size=[3, 3], dropout_p=0.2, name=N
 
     #conv = slim.conv2d(preact, n_filters, kernel_size, activation_fn=None, normalizer_fn=None,
     #                   weights_initializer=tf.random_normal_initializer(stddev=0.02, seed=4285), biases_initializer=tf.constant_initializer(0.01))
-    conv = conv2d(preact, n_filters, k_h=kernel_size[0], k_w=kernel_size[1], d_h=1, d_w=1, use_spectral_norm=True, name=name)
+    conv = conv2d(preact, n_filters, k_h=kernel_size[0], k_w=kernel_size[1], d_h=1, d_w=1, use_spectral_norm=True, name=name + "_co")
 
     if dropout_p != 0.0:
-        conv = slim.dropout(conv, keep_prob=(1.0 - dropout_p))
+        conv = slim.dropout(conv, keep_prob=(1.0 - dropout_p), scope=name + "_do")
 
     return conv
 
 
-def DenseBlock(stack, n_layers, growth_rate, dropout_p, isDec=False, scope=None):
+def DenseBlock(stack, n_layers, growth_rate, dropout_p, isDec=False, scope=None, model=None):
   """
   DenseBlock for DenseNet and FC-DenseNet
   Arguments:
@@ -57,12 +63,18 @@ def DenseBlock(stack, n_layers, growth_rate, dropout_p, isDec=False, scope=None)
   """
   with tf.name_scope(scope):
     new_features = []
-    for i in range(n_layers):
+    layers = n_layers if isDec or model != 'FC-DenseNet-RF-46' else n_layers + 4
+    for i in range(layers):
+      kernel = [3, 3]
+      if model == 'FC-DenseNet-RF-46':
+        # because of small RF, only use 3x3 kernel in first layer of DB, then only 1x1 to keep the RF size constant
+        if i > 0:
+            kernel = [1, 1]
       # Compute new feature maps
       if isDec:
-        layer = preact_conv_dec(stack, growth_rate, dropout_p=dropout_p, name=scope + '_' + str(i))
+        layer = preact_conv_dec(stack, growth_rate, kernel_size=kernel, dropout_p=dropout_p, name=scope + '_' + str(i))
       else:
-        layer = preact_conv(stack, growth_rate, dropout_p=dropout_p)
+        layer = preact_conv(stack, growth_rate, kernel_size=kernel, dropout_p=dropout_p, scope=scope + '_' + str(i))
       new_features.append(layer)
       # Stack new layer
       stack = tf.concat([stack, layer], axis=-1)
@@ -76,8 +88,8 @@ def TransitionDown(inputs, n_filters, dropout_p=0.2, scope=None):
   Apply 1x1 BN + ReLU + conv then 2x2 max pooling
   """
   with tf.name_scope(scope) as _:
-    l = preact_conv(inputs, n_filters, kernel_size=[1, 1], dropout_p=dropout_p)
-    l = slim.pool(l, [2, 2], stride=[2, 2], pooling_type='MAX')
+    l = preact_conv(inputs, n_filters, kernel_size=[1, 1], dropout_p=dropout_p, scope=scope)
+    l = slim.pool(l, [2, 2], stride=[2, 2], pooling_type='MAX', scope=scope + "_po")
     return l
 
 
@@ -96,7 +108,7 @@ def TransitionUp(block_to_upsample, n_filters_keep, batch_size, out_res, scope=N
     # l = deconv2d(block_to_upsample, [batch_size, out_res, out_res, n_filters_keep], k_h=3, k_w=3, d_h=2, d_w=2, padding='SAME', use_spectral_norm=True, name='g_'+scope)
 
     in_channels = block_to_upsample.get_shape()[-1]
-    l = resize_conv(block_to_upsample, in_channels, n_filters_keep, 3, [1, 2, 2, 1], use_spectral_norm=True, name='g_'+scope)
+    l = resize_conv(block_to_upsample, in_channels, n_filters_keep, 3, [1, 2, 2, 1], use_spectral_norm=True, name=scope)
 
     # print('after:', l.shape)
     # print('TransitionUp <--')
@@ -106,19 +118,18 @@ def TransitionUp(block_to_upsample, n_filters_keep, batch_size, out_res, scope=N
     return l
 
 
-def encoder_dense(inputs, batch_size, feature_size, n_filters_first_conv=48, preset_model='FC-DenseNet56', dropout_p=0.2, scope='g_1_enc'):
+def encoder_dense(inputs, batch_size, feature_size, n_filters_first_conv=48, preset_model='FC-DenseNet56', dropout_p=0.2, addCoordConv=False, scope='g_1_enc'):
     """
     Builds the FC-DenseNet model
 
     Arguments:
       inputs: the input tensor
+      batch_size:
+      feature_size:
       preset_model: The model you want to use
-      n_classes: number of classes
       n_filters_first_conv: number of filters for the first convolution applied
-      n_pool: number of pooling layers = number of transition down = number of transition up
-      growth_rate: number of new feature maps created by each layer in a dense block
-      n_layers_per_block: number of layers per block. Can be an int or a list of size 2 * n_pool + 1
       dropout_p: dropout rate applied after each convolution (0. for not using)
+      addCoordConv:
       scope: scope or name
 
     Returns:
@@ -126,7 +137,15 @@ def encoder_dense(inputs, batch_size, feature_size, n_filters_first_conv=48, pre
     """
     # print('encoder_dense -->')
 
-    if preset_model == 'FC-DenseNet56':
+    skipDbId = None
+    if preset_model == 'FC-DenseNet-RF-46':
+      # RF:
+      n_pool=4
+      n_filters_first_conv = 84
+      growth_rate=104
+      skipDbId = 3 # because of RF size
+      n_layers_per_block=1 # note well: in function DenseBlock ann additional 4 1x1 conv layers are added in case of encoder
+    elif preset_model == 'FC-DenseNet56':
       # FC-DenseNet56: 56 layers, with 4 layers per dense block and a growth rate of 12
       n_pool=5
       growth_rate=12
@@ -148,6 +167,8 @@ def encoder_dense(inputs, batch_size, feature_size, n_filters_first_conv=48, pre
     elif type(n_layers_per_block) == int:
         n_layers_per_block = [n_layers_per_block] * n_pool
 
+    print('n_layers_per_block: %s' % n_layers_per_block)
+
     with tf.variable_scope(scope, preset_model, [inputs]) as sc:
 
       #####################
@@ -155,6 +176,14 @@ def encoder_dense(inputs, batch_size, feature_size, n_filters_first_conv=48, pre
       #####################
       # We perform a first convolution.
       # print('inputs before conv: ', inputs.shape)
+      if addCoordConv:
+          # add a CoordConv layer at the beginning of encoder
+          bshape = inputs.get_shape()
+          inputs = coord_conv(inputs)
+          assert inputs.get_shape()[3] == bshape[3] + 2
+          assert inputs.get_shape()[1] == bshape[1]
+          assert inputs.get_shape()[2] == bshape[2]
+
       stack = slim.conv2d(inputs, n_filters_first_conv, [3, 3], scope='first_conv', activation_fn=None,
                           weights_initializer=tf.random_normal_initializer(stddev=0.02, seed=4285), biases_initializer=tf.constant_initializer(0.01))
       # print('stack after conv: ', stack.shape)
@@ -167,24 +196,38 @@ def encoder_dense(inputs, batch_size, feature_size, n_filters_first_conv=48, pre
 
       for i in range(n_pool):
         # Dense Block
-        stack, _ = DenseBlock(stack, n_layers_per_block[i], growth_rate, dropout_p, scope='denseblock%d' % (i+1))
-        # print('stack after DB %d: %s' % (i+1, str(stack.shape)))
+        if skipDbId is None or i != skipDbId:
+            print('DB-%d' % i)
+            stack, _ = DenseBlock(stack, n_layers_per_block[i], growth_rate, dropout_p, scope='DB-%d' % (i+1), model=preset_model)
+            print('stack after DB %d: %s' % (i+1, str(stack.shape)))
         n_filters += growth_rate * n_layers_per_block[i]
 
         # Transition Down
         # print('n_filters before TUP %d: %d' % (i+1, n_filters))
-        stack = TransitionDown(stack, n_filters, dropout_p, scope='transitiondown%d'%(i+1))
-        # print('stack after TD %d: %s' % (i+1, str(stack.shape)))
+        print('TD-%d' % i)
+        stack = TransitionDown(stack, n_filters, dropout_p, scope='TD-%d'%(i+1))
+        print('stack after TD %d: %s' % (i+1, str(stack.shape)))
 
-      # 1x1 convolution to reduce channel dimension from 288 to 192
-      net = slim.conv2d(stack, 192, [1, 1], activation_fn=None, scope='logits',
-                        weights_initializer=tf.random_normal_initializer(stddev=0.02, seed=4285), biases_initializer=tf.constant_initializer(0.01))
 
-      # print('net before reshape:', net.shape)
-      net = tf.reshape(net, [batch_size, -1])
-      # print('net before reshape:', net.shape)
+      print('stack.name: %s' % stack.name)
+      print('stack shape:', stack.shape)
+
+      if preset_model == 'FC-DenseNet-RF-46':
+        # reduce channel dimension from 288 to 192 and resolution 2x2
+        print('TD-final')
+        net = TransitionDown(stack, 192, dropout_p, scope='TD-final')
+      else:
+        # 1x1 convolution to reduce channel dimension from 288 to 192
+        net = slim.conv2d(stack, 192, [1, 1], activation_fn=None, scope='logits',
+                          weights_initializer=tf.random_normal_initializer(stddev=0.02, seed=4285), biases_initializer=tf.constant_initializer(0.01))
+
+      print('net before name:', net.name)
+      print('net before reshape:', net.shape)
+      net = tf.reshape(net, [batch_size, -1], name='output')
+      print('net before reshape:', net.shape)
 
       assert net.shape[0] == batch_size
+      print('feature_size: %d' % feature_size)
       assert net.shape[1] == feature_size
 
       # print('encoder_dense <--')
@@ -214,7 +257,14 @@ def decoder_dense(inputs, batch_size, feature_size, n_filters_first_conv=48, pre
     """
     # print('decoder_dense -->')
 
-    if preset_model == 'FC-DenseNet56':
+    if preset_model == 'FC-DenseNet-RF-46':
+      # RF of 46, with simplified DenseNet
+      n_pool = 4
+      growth_rate = 48
+      n_layers_per_block = [5] * (2 * n_pool + 1)
+      n_filters_to_keep = [-1, -1, -1, -1, -1, 520, 404, 302, 208]
+      conv2d_res = [-1, -1, -1, -1, -1, 4, 8, 16, 32, 64]
+    elif preset_model == 'FC-DenseNet56':
       # FC-DenseNet56: 56 layers, with 4 layers per dense block and a growth rate of 12
       n_pool=5
       growth_rate=12
@@ -267,30 +317,25 @@ def decoder_dense(inputs, batch_size, feature_size, n_filters_first_conv=48, pre
       #######################
 
       for i in range(n_pool):
-        # Self-attention layer (from SAGAN):
-        # if i == 4: # before last DB add self-attention layer
-        #     #################################
-        #     assert n_pool == 5 # designed for case of 5 DBs
-        #     print('add self-attention layer SA.. [n_filters_keep: %d]' % block_to_upsample.get_shape()[-1])
-        #     block_to_upsample = attention(block_to_upsample, block_to_upsample.get_shape()[-1], sn=True, scope="d_attention", reuse=reuse)
-        #     print('block_to_upsample after SA: ', block_to_upsample.shape)
-        #     #################################
-
         # Transition Up ( Upsampling + concatenation with the skip connection)
         n_filters_keep = n_filters_to_keep[n_pool + i + 1]
         out_res = conv2d_res[n_pool + i + 1]
-        # print('n_filters_keep TUP: %d' % n_filters_keep)
-        stack = TransitionUp(block_to_upsample, n_filters_keep, batch_size, out_res, scope='transitionup_%d' % (n_pool + i + 1))
-        # print('stack after TUP %d: %s' % (i+1, str(stack.shape)))
+        print('n_filters_keep TUP: %d' % n_filters_keep)
+        stack = TransitionUp(block_to_upsample, n_filters_keep, batch_size, out_res, scope='TUP-%d' % (n_pool + i + 1))
+        print('stack after TUP %d: %s' % (i+1, str(stack.shape)))
 
         # Dense Block
         # We will only upsample the new feature maps
         n_layers_next = n_layers_per_block[n_pool + i + 1]
 
-        # print('n_layers_next DB: %d' % n_layers_next)
-        stack, block_to_upsample = DenseBlock(stack, n_layers_next, growth_rate, dropout_p, isDec=True, scope='denseblock_%d' % (n_pool + i + 2))
-        # print('stack after DB %d: %s' % (i+1, str(stack.shape)))
-        # print('block_to_upsample after DB %d: %s' % (i+1, str(block_to_upsample.shape)))
+        print('n_layers_next DB: %d' % n_layers_next)
+        stack, block_to_upsample = DenseBlock(stack, n_layers_next, growth_rate, dropout_p, isDec=True, scope='DB-%d' % (n_pool + i + 2), model=preset_model)
+        print('stack after DB %d: %s' % (i+1, str(stack.shape)))
+        print('block_to_upsample after DB %d: %s' % (i+1, str(block_to_upsample.shape)))
+
+      if preset_model == 'FC-DenseNet-RF-46':
+          stack = TransitionUp(block_to_upsample, 192, batch_size, 64, scope='TUP-final')
+          print('stack after TUP final: %s' % str(stack.shape))
 
       num_colors = 3
       # 1x1 convolution to bring down 3rd dimension from 96 to 3
@@ -303,7 +348,6 @@ def decoder_dense(inputs, batch_size, feature_size, n_filters_first_conv=48, pre
       assert net.shape[3] == 3
 
       # print('decoder_dense <--')
-
 
       return tf.nn.tanh(net)
 
