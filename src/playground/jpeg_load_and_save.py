b104f8d03e7d25cd0cde42c5417e8af9de47b53b
lukaszbinden <lukas.zbinden@unifr.ch>
2019-03-27 12:08:37 +0100

    test jpeg different save methods -> best is PNG + ResizeMethod.AREA

--- src/playground/jpeg_load_and_save.py
+++ src/playground/jpeg_load_and_save.py
@@ -4,6 +4,13 @@ import scipy
 from utils_dcgan import save_images, inverse_transform
 
 
+def prep(orig):
+    orig = tf.reshape(orig, (64, 64, 3))
+    orig = tf.expand_dims(orig, 0)
+    orig = tf.cast(orig, tf.float32) * (2. / 255) - 1
+    return orig
+
+
 with tf.Session() as sess:
     tf.set_random_seed(4285)
 
@@ -14,31 +21,40 @@ with tf.Session() as sess:
     print("file:", file)
 
     orig = tf.read_file("../tools/orig_000000177006.jpg")
-    orig = tf.decode_raw(features['image'], tf.uint8)
-    # image.set_shape([img_size * img_size * img_channels])
     orig = tf.image.decode_jpeg(orig)
     orig = tf.image.flip_left_right(orig)
     size = tf.minimum(427, 640)
     crop_shape = tf.parallel_stack([size, size, 3])
     orig = tf.random_crop(orig, crop_shape, seed=4285)
-    orig = tf.image.resize_images(orig, [64, 64])
-    orig = tf.reshape(orig, (64, 64, 3))
-    orig = tf.expand_dims(orig, 0)
-    orig = tf.cast(orig, tf.float32) * (2. / 255) - 1
+    origimg = orig
+    orig = None
+
+    orig = tf.image.resize_images(origimg, [64, 64], method=tf.image.ResizeMethod.BILINEAR)
+    orig = prep(orig)
     print("orig:", orig)
+    orignn = tf.image.resize_images(origimg, [64, 64], method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+    orignn = prep(orignn)
+    origbi = tf.image.resize_images(origimg, [64, 64], method=tf.image.ResizeMethod.BICUBIC)
+    origbi = prep(origbi)
+    origar = tf.image.resize_images(origimg, [64, 64], method=tf.image.ResizeMethod.AREA)
+    origar = prep(origar)
 
     sess.run(tf.global_variables_initializer())
     sess.run(tf.local_variables_initializer())
 
-    file, orig = sess.run([file, orig])
+    file, orig, nn, bi, ar = sess.run([file, orig, orignn, origbi, origar])
 
 
-    scipy.misc.imsave("out/jpeg_load_and_save_1.jpg", file[0])
+    scipy.misc.imsave("out/jpeg_load_and_save_1.png", file[0])
     file2 = inverse_transform(file)
-    scipy.misc.imsave("out/jpeg_load_and_save_2.jpg", file2[0])
+    scipy.misc.imsave("out/jpeg_load_and_save_2.png", file2[0])
 
-    save_images(file, [1,1], "out/jpeg_load_and_save_3.jpg")
+    save_images(file, [1,1], "out/jpeg_load_and_save_3.png")
 
-    save_images(file, [1,1], "out/jpeg_load_and_save_4.jpg", invert=False)
+    save_images(file, [1,1], "out/jpeg_load_and_save_4.png", invert=False)
 
-    scipy.misc.imsave("out/jpeg_load_and_save_5_orig.jpg", orig[0])
+    scipy.misc.imsave("out/jpeg_load_and_save_5_orig.png", orig[0])
+    scipy.misc.imsave("out/jpeg_load_and_save_6_nn.png", nn[0])
+    scipy.misc.imsave("out/jpeg_load_and_save_7_bi.png", bi[0])
+    scipy.misc.imsave("out/jpeg_load_and_save_8_ar.png", ar[0])
+
